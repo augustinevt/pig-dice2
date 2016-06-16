33dc4564e1ba9dd34b1ac2d21c6db000a0e0ef1e
August von Trapp and Wolfgang Warneke <august@>
2016-06-16 10:59:34 -0700

    add start click start video event

---
+++ README.md
@@ -0,0 +1,20 @@
+SPECS
+
+User presses start a start video plays.X
+
+Blank video is loaded after start video.
+
+User presses roll and receives random die value.
+Ex: 5.
+
+Random value cues video.
+
+Rolled die shows up in current turn bank.
+
+Video reverts back to blank video.
+
+If value rolled is one turn is over.
+
+When user clicks hold turn total is added to score and turn is over.
+
+When turn total is added to score, game is won.
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
+var turn;
+
+
 $(function(){
-  alert('herronn')
+  $('#start').click(function() {
+    //start video
+    var videoFile = 'vid/start.mp4';
+    $('#videocontainer video source').attr('src', videoFile);
+    $("#videocontainer video")[0].load();
+  });
+
+  $('#roll').click(function() {});
+    if (turn === "player") {
+      //player stuff
+    }
 });
 
-for the general UI
-
-
-for the rolling UI
-{value: 6 video_uri: "../vid/roll6.mp4", img: "../img/six.jpg"}, {temScore: 6}, {permScore: 40,  }
+// for the general UI
+//
+//
+// for the rolling UI
+// {value: 6 video_uri: "../vid/roll6.mp4", img: "../img/six.jpg"}, {temScore: 6}, {permScore: 40,  }
