6d09e339062c35826aa234a97dee2853ecf14129
August von Trapp and Wolfgang Warneke <august@>
2016-06-16 12:24:24 -0700

    add die object and roll functionality

--- README.md
+++ README.md
@@ -2,16 +2,16 @@ SPECS
 
 User presses start a start video plays.X
 
-Blank video is loaded after start video.
+Blank video is loaded after start video.X
 
-User presses roll and receives random die value.
+User presses roll and receives random die value.X
 Ex: 5.
 
-Random value cues video.
+Random value cues video.X
 
 Rolled die shows up in current turn bank.
 
-Video reverts back to blank video.
+Video reverts back to blank video.X
 
 If value rolled is one turn is over.
 
@@ -23,5 +23,7 @@ When turn total is added to score, game is won.
 Backend
 
   have hold switch the player X
+X
+# will alternate player and assign turn to new turn instance X
 
- 
+# call roll on turn currentTurn X
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,40 @@
 var turn = true;
 
+//// turn
+function Turn(context) {
+  this.player = context;
+  this.tempScore = 33;
+}
+
+Turn.prototype.roll = function() {
+  var die = dieValues[Math.floor( Math.random() * 6)];
+
+  console.log(die);
+  this.tempScore += die.value;
+  alert(this.tempScore);
+  return die;
+}
+
+
+////player
 function Player(name) {
   this.name = name;
 }
 
-Player.prototype.roll = function() {
-  return this.name;
+Player.prototype.turn = function() {
+  return new Turn(this);
 }
+
+
+
+///////
 
 
 var playerOne = new Player("player one");
 var playerTwo = new Player("player two");
 
 var currentPlayer;
+var currentTurn;
 
 function turnOver() {
   if (turn) {
@@ -24,15 +46,45 @@ function turnOver() {
   }
 }
 
+function newTurn() {
+  return currentTurn = currentPlayer.turn();
+}
 
+
+///die object stuff
+var dieValues = [
+  {value: 1, video: "vid/roll1.mp4", image: "img/one.png"},
+  {value: 2, video: "vid/roll2.mp4", image: "img/two.jpg"},
+  {value: 3, video: "vid/roll3.mp4", image: "img/three.jpg"},
+  {value: 4, video: "vid/roll4.mp4", image: "img/four.jpg"},
+  {value: 5, video: "vid/roll5.mp4", image: "img/five.jpg"},
+  {value: 6, video: "vid/roll6.mp4", image: "img/six.jpg"}
+];
 
 //////
 
 $(function(){
   turnOver();
+  newTurn();
+
   $('#start').click(function() {
+
     //start video
     var videoFile = 'vid/start.mp4';
+    $('#videocontainer video source').attr('src', videoFile);
+    $("#videocontainer video")[0].load();
+    $('#vid').one('ended', function() {
+
+      var videoFile = 'vid/blank.mp4';
+      $('#videocontainer video source').attr('src', videoFile);
+      $("#videocontainer video")[0].load();
+    });
+  });
+
+  $('#roll').click(function() {
+
+    var die = currentTurn.roll();
+    var videoFile = die.video;
     $('#videocontainer video source').attr('src', videoFile);
     $("#videocontainer video")[0].load();
     $('#vid').one('ended', function() {
@@ -42,11 +94,9 @@ $(function(){
     });
   });
 
-  $('#roll').click(function() {
-    alert(currentPlayer.roll());
-  });
-
   $('#hold').click(function() {
+    newTurn();
+    alert(currentTurn.player.name);
     turnOver();
   });
 
