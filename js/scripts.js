105200995da2742805ad97eb6607cf460dfa2974
August von Trapp and Wolfgang Warneke <august@>
2016-06-16 09:58:50 -0700

    add basic HTML div layout for UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(function(){
   alert('herronn')
 });
+
+for the general UI
+
+
+for the rolling UI
+{value: 6 video_uri: "../vid/roll6.mp4", img: "../img/six.jpg"}, {temScore: 6}, {permScore: 40,  }
